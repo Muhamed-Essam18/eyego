72149284d5bb04996108139db7b1748d0a451c55
Muhamed Essam <muhamed.essam2@aiesec.net>
2026-03-05 03:57:14 +0200

    feat(core): setup redux store and provider

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,1 +1,8 @@
-// Redux provider component
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,8 @@
-// Redux store configuration
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
