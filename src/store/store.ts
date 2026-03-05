161cd2c6b3c2c48c1d20b53d8f0d763a2733a153
Muhamed Essam <muhamed.essam2@aiesec.net>
2026-03-05 20:08:02 +0200

    setup redux store and employees slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import employeesSlice from "@/features/employees/employeesSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    employees: employeesSlice,
+  },
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
